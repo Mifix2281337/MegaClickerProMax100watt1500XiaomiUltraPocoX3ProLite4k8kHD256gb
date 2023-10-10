eefb5f2d8f0010bb5c742860b82e3d714bca7305
Mifix2281337 <makaridanil1064@gmail.com>
2023-10-10 20:46:25 +0500

    sound and time update

--- final.py
+++ final.py
@@ -7,6 +7,7 @@ from PyQt5.QtCore import *
 from PyQt5 import uic
 from playsound import playsound
 from PIL import Image
+from PyQt5 import QtTest
 import time
 import random
 
@@ -33,11 +34,8 @@ class Boss(QMainWindow):
     def clickedCheck(self):
         self.clicks_count += 1
         self.clck.setText(str(self.clicks_count))
+        playsound("sound\\udar2.mp3", False)
         self.btn.move(random.randint(20, 480),random.randint(290, 480))
-        image = Image.open("macboss\\1.png")
-        new_image = image.resize((200, 200))
-        new_image.save('new_image.png')
-        self.lbll2.setPixmap(QPixmap("new_image.png"))
 
     # регистратор промаха
     def mousePressEvent(self, event):
@@ -47,7 +45,12 @@ class Boss(QMainWindow):
         new_image = image.resize((200, 200))
         new_image.save('new_image.png')
         self.lbll2.setPixmap(QPixmap("new_image.png"))
-
+        playsound("sound\\udar.mp3", False)
+        QtTest.QTest.qWait(100)
+        image = Image.open("macboss\\1.png")
+        new_image = image.resize((200, 200))
+        new_image.save('new_image.png')
+        self.lbll2.setPixmap(QPixmap("new_image.png"))
         if len(self.hp_indicator) == 0:
             exit()
 
